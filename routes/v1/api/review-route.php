6c7ef34544a1ba0ce4d2b763045a97bd02ae21ae
keshiecodes <morahthankgod@gmail.com>
2022-11-22 18:35:07 +0100

    typo correct in review route

--- routes/v1/api/review-route.php
+++ routes/v1/api/review-route.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
 
     Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/read-reviews', 'read');
-        Route::post('/create-review', 'create');
-        Route::post('/read-review-by-id', 'readById');
-        Route::post('/read-product-review', 'readByProductId');
+        Route::controller(ReviewController::class)->group(function () {
+            Route::get('/read-reviews', 'read');
+            Route::post('/create-review', 'create');
+            Route::post('/read-review-by-id', 'readById');
+            Route::post('/read-product-review', 'readByProductId');
+        });
     });
 });
